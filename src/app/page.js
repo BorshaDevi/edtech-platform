41ff3ae20da32bb015e09b8dd9fd8303fa9e2abb
Borsha Devi <borshadevi27@gmail.com>
2025-12-03 15:53:09 +0600

    create CourseSection.jsx file

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import CourseSection from "@/component/CourseSection";
 
 
 export default function Home() {
   return (
     <div className="mx-0 container px-2">
-      <h1>home</h1>
+      <CourseSection></CourseSection>
     </div>
   );
 }
