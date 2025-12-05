b90ebea533708fbb56c44344b3cbb5604208d1df
Borsha Devi <borshadevi27@gmail.com>
2025-12-05 15:28:56 +0600

    install hover card and avatar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,16 +1,37 @@
 import Link from "next/link";
-
+import { Avatar, AvatarFallback } from "@/components/ui/avatar"
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card"
+import { Button } from "@/components/ui/button";
 export default function Navbar() {
+  const user = true;
   return (
     <div className="max-w-full h-14 shadow-lg rounded-md">
-        <div className="p-4 w-full">
-          <div className="flex justify-between px-3">
-            <h1 className="uppercase text-2xl font-semibold text-cyan-500">EDTech Platform</h1>
-            <Link href='/' className={`text-xl`}>Home</Link>
-            {user? '': <Link href='/login' className="text-xl">Join us</Link>}
-            
-          </div>
+      <div className="p-4 w-full">
+        <div className="flex justify-between px-3">
+          <h1 className="uppercase text-2xl font-semibold text-cyan-500">EDTech Platform</h1>
+          <Link href='/' className={`text-xl`}>Home</Link>
+          {user ?
+            <HoverCard>
+              <HoverCardTrigger>
+                <Avatar>
+                  <AvatarFallback className='hover:cursor-pointer'>CN</AvatarFallback>
+                </Avatar>
+              </HoverCardTrigger>
+              <HoverCardContent>
+                <div className="grid grid-cols-1">
+                  <Link href='' >Dashborad</Link>
+                <Button className='mt-3'>Logout</Button>
+                </div>
+              </HoverCardContent>
+            </HoverCard>
+
+            : <Link href='/login' className="text-xl">Join us</Link>}
         </div>
+      </div>
     </div>
   );
 }
