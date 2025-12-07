c52a1214431c298568979502dc9f943d9eb04c63
Borsha Devi <borshadevi27@gmail.com>
2025-12-07 23:39:26 +0600

    added text in Dashboard.jsx file and install React Icon

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,11 +1,13 @@
 import Dashboard from "@/component/Dashboard";
+import { SidebarProvider } from "@/components/ui/sidebar";
 
 export default function DashboardLayout({ children }) {
   return (
     <div className="min-h-screen flex">
-     
+     <SidebarProvider>
       <Dashboard></Dashboard>
       {children}
+      </SidebarProvider>
     </div>
   );
 }
--- src/component/Dashboard.jsx
+++ src/component/Dashboard.jsx
@@ -1,7 +1,32 @@
-const Dashboard=()=>{
-    return(
+import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuItem, SidebarRail } from "@/components/ui/sidebar";
+import Link from "next/link";
+
+const Dashboard = () => {
+    return (
         <div>
-            <h1 className="text-green-600 font-bold">Dashboard</h1>
+
+            <Sidebar collapsible="icon" >
+                <SidebarHeader>
+                    <h1 className="text-green-600 font-bold">Dashboard</h1>
+                </SidebarHeader>
+                <SidebarContent>
+                    <SidebarMenu>
+                        <SidebarMenuItem>
+                            <Link href=''> <span> </span>Student</Link>
+                        </SidebarMenuItem>
+                        <SidebarMenuItem>
+                            <Link href=''> Student</Link>
+                        </SidebarMenuItem>
+                        <SidebarMenuItem>
+                            <Link href=''> Student</Link>
+                        </SidebarMenuItem>
+                    </SidebarMenu>
+                </SidebarContent>
+                <SidebarFooter>
+                    <p>Footer</p>
+                </SidebarFooter>
+                <SidebarRail />
+            </Sidebar>
         </div>
     )
 }
