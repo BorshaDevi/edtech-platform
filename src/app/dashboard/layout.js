03e42cd44fa3cfabe9e8e9a64dcce5a0d2c604e4
Borsha Devi <borshadevi27@gmail.com>
2025-12-07 22:54:54 +0600

    install sidebar

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,6 +1,10 @@
+import Dashboard from "@/component/Dashboard";
+
 export default function DashboardLayout({ children }) {
   return (
     <div className="min-h-screen flex">
+     
+      <Dashboard></Dashboard>
       {children}
     </div>
   );
---
+++ src/component/Dashboard.jsx
@@ -0,0 +1,8 @@
+const Dashboard=()=>{
+    return(
+        <div>
+            <h1 className="text-green-600 font-bold">Dashboard</h1>
+        </div>
+    )
+}
+export default Dashboard;
