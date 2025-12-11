31048a56141b6241ec885da6bd1bd3cd609a97ed
Borsha Devi <borshadevi27@gmail.com>
2025-12-11 20:40:28 +0600

    create bannar section and add this in home page

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Bannar from "@/component/Bannar";
 
 export default function Home() {
   return (
-    <div className="mx-0 container px-2">
+    <div className="mx-0 container px-2 ">
       <Bannar></Bannar>
     </div>
   );
--- src/component/Bannar.jsx
+++ src/component/Bannar.jsx
@@ -1,7 +1,39 @@
+'use client'
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import './styles.css';
+
+// import required modules
+import { Autoplay, Pagination } from 'swiper/modules';
 const Bannar=()=>{
     return(
-        <div>
-            <p>Bannar</p>
+        <div className='mt-10 md:ml-10'>
+            <h1 className='text-center font-semibold text-2xl text-cyan-700'> Course Discount</h1>
+            <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        // navigation={true}
+        modules={[Autoplay, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide><img  src='https://res.cloudinary.com/dg1okcxsx/image/upload/v1764839121/programming-background-with-person-working-with-codes-computer_dg5zi6.jpg'></img></SwiperSlide>
+        <SwiperSlide><img src='https://res.cloudinary.com/dg1okcxsx/image/upload/v1764839121/programming-background-with-person-working-with-codes-computer_dg5zi6.jpg'></img></SwiperSlide>
+        <SwiperSlide><img src='https://res.cloudinary.com/dg1okcxsx/image/upload/v1764839121/programming-background-with-person-working-with-codes-computer_dg5zi6.jpg'></img></SwiperSlide>
+        <SwiperSlide><img src='https://res.cloudinary.com/dg1okcxsx/image/upload/v1764839121/programming-background-with-person-working-with-codes-computer_dg5zi6.jpg'></img></SwiperSlide>
+      </Swiper>
+
         </div>
     )
 }
