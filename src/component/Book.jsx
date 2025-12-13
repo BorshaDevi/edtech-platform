06d8918912b501a3892a06742eaee859c3d34131
Borsha Devi <borshadevi27@gmail.com>
2025-12-13 23:22:23 +0600

    added text

--- src/component/Book.jsx
+++ src/component/Book.jsx
@@ -2,6 +2,9 @@ const Book=()=>{
     return(
         <div className='mt-10 md:ml-10'>
           <h1 className='text-center font-semibold text-2xl text-cyan-700 mb-5 underline underline-offset-2'>Our Books</h1>
+          <div>
+            
+          </div>
         </div>
     )
 }
