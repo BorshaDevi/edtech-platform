c49e6f13dee52b66ee904c42611f6d2b47166c27
Borsha Devi <borshadevi27@gmail.com>
2025-12-14 22:36:17 +0600

    added some design in Book.jsx file

--- src/component/Book.jsx
+++ src/component/Book.jsx
@@ -3,7 +3,8 @@ const Book=()=>{
         <div className='mt-10 md:ml-10'>
           <h1 className='text-center font-semibold text-2xl text-cyan-700 mb-5 underline underline-offset-2'>Our Books</h1>
           <div>
-            
+            <img src=''></img>
+            <h1></h1>
           </div>
         </div>
     )
