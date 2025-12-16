e86ddf74fb980656ec31c3e8ccd7270870b55a9f
Borsha Devi <borshadevi27@gmail.com>
2025-12-16 23:36:07 +0600

    in book.jsx file I add json data for show books

--- src/component/Book.jsx
+++ src/component/Book.jsx
@@ -1,12 +1,37 @@
-const Book=()=>{
-    return(
-        <div className='mt-10 md:ml-10'>
-          <h1 className='text-center font-semibold text-2xl text-cyan-700 mb-5 underline underline-offset-2'>Our Books</h1>
-          <div>
-            <img src=''></img>
-            <h1></h1>
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card"
+import books from "@/data/book.json";
+const Book = () => {
+  return (
+    <div className='mt-10 md:ml-10'>
+      <h1 className='text-center font-semibold text-2xl text-cyan-700 mb-5 underline underline-offset-2'>Our Books</h1>
+      <div className="flex gap-5">
+        {
+           books.map(book => (
+            <div key={book.id}>
+              {/* Hover card */}
+          <HoverCard>
+        <HoverCardTrigger>
+          {/* card */}
+          <div className="card">
+            <img className='rounded-b-md p-1 ' src='https://res.cloudinary.com/dg1okcxsx/image/upload/v1764839127/html-css-collage-concept-with-person_xhj7ai.jpg'></img>
+            <h1 className="text-2xl font-medium">{book.title}</h1>
           </div>
-        </div>
-    )
+        </HoverCardTrigger>
+        <HoverCardContent className="w-60">
+          <h1 className="text-xl space-x-2"><span >Author:</span><span className="text-cyan-700">{book.author}</span></h1>
+          <p className="text-xl space-x-2"><span >Price:</span><span className="text-green-500">{book.price}TK.</span></p>
+        </HoverCardContent>
+      </HoverCard>
+            </div>
+           ))
+        }
+      </div>
+
+    </div>
+  )
 }
 export default Book;
