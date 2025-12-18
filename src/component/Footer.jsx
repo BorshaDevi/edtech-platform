86aa783985d00d8c76e6e82a0298e1082ad7c0c7
Borsha Devi <borshadevi27@gmail.com>
2025-12-18 23:31:57 +0600

    added some text in footer .jsx file

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,6 +1,12 @@
 const Footer=()=>{
     return(
         <div className='mt-10 md:ml-10'>
+            <div className="w-full ">
+              <div className="grid grid-cols-4">
+                <p></p>
+                
+              </div>
+            </div>
            <p>Footer</p>
         </div>
     )
