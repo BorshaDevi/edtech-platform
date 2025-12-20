a70068f4bc7ffde93434165981790d3fd72e51b5
Borsha Devi <borshadevi27@gmail.com>
2025-12-20 21:40:12 +0600

    complete footer section

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,13 +1,17 @@
 const Footer=()=>{
     return(
-        <div className='mt-10 md:ml-10'>
-            <div className="w-full ">
-              <div className="grid grid-cols-4">
-                <p></p>
-                
+        <div className='mt-10 md:ml-10 max-w-full md:pr-17  mx-auto h-auto '>
+            <div className="bg-linear-to-r from-cyan-900 to-cyan-200  text-center">
+              <div className="grid grid-cols-2 p-8">
+                <h1 className="uppercase text-2xl font-semibold text-cyan-500">EDTech Platform</h1>
+                <div className="text-start text-xl font-semibold">
+                  <p><span className="text-white">Phone</span>:0180000000</p>
+                  <p><span className="text-white">Email</span>:edtech@platform.com</p>
+                </div>
               </div>
+              <p className="text-center text-xl text-white">copyright@EdTech Platform 2025</p>
             </div>
-           <p>Footer</p>
+           
         </div>
     )
 }
